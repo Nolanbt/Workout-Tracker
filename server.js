7ab4b672e3472cc8da2f9c37862147d34006685f
Nolan Terpstra <nbakert@gmail.com>
2020-09-24 17:36:45 -0500

    deploying

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 
 require("./routes/html_routes")(app);
